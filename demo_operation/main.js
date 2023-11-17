114f5e49bc52e3d071feac9e1705b4e8d8c7fef4
Jaber kh <97162517+Jabkh@users.noreply.github.com>
2023-11-17 16:27:53 +0100

    commit exo 2 addition

---
+++ demo_operation/main.js
@@ -0,0 +1,24 @@
+
+let value1 = prompt("Saisir nb1");
+
+let value2 = prompt("Saisir nb2");
+
+let result;
+ // addition
+result = value1 + value2;
+
+let message2 = `Votre résultat est ${result}`;
+
+let message1 = `Votre Calcul est ${value2} + ${value2}`;
+
+document.getElementById("1").textContent = message1;
+
+document.getElementById("2").textContent = message2;
+
+// soustraction
+// result = value1 - value2;
+
+// document.getElementById("1").textContent = message1;
+
+// document.getElementById("2").textContent = message2;
+
