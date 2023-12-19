a064da15fc0259493754f8344c32d9f9bfb69248
Jaber kh <97162517+Jabkh@users.noreply.github.com>
2023-12-19 15:08:17 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -17,24 +17,6 @@ const routes = [
     name: 'Signin',
     component: RegisterView
   },
-  // {
-  //   path: '/characters',
-  //   name: 'Characters',
-  //   component: CharacterView,
-  //   beforeEnter: requireAuth
-  //   // router.beforeEach(async (to, from) => {
-  //   //   if (
-  //   //     // make sure the user is authenticated
-  //   //     !isLoggedIn &&
-  //   //     // ❗️ Avoid an infinite redirect
-  //   //     to.name !== 'Characters'
-  //   //   ) {
-  //   //     // redirect the user to the login page
-  //   //     return { name: 'Characters' }
-  //   //   }
-  //   // })
-  // },
-
   {
     path: '/characters',
     name: 'Characters',
@@ -55,72 +37,4 @@ const router = createRouter({
   routes,
 });
 
-// router.beforeEach((to,from)=>{
-//   if(to.path.startsWith("/characters")){
-//     const {user} = inject("user");
-//     if(user.value) return true;
-//     else return "/";
-//   }else return true;
-// });
-
-;
-
-// router.beforeEach((to, from, next) => {
-//   if (to.name !== 'Character' && !isLoggedIn) next({ name: 'Character' })
-//   else next()
-// });
-
 export default router;
-
-
-// import { useAuthStore } from './store/authStore'
-
-// export function requireAuth(to, from, next) {
-//   const authStore = useAuthStore()
-
-//   if (authStore.isAuthenticated) {
-//     next()
-//   } else {
-//     next('/login')
-//   }
-// }
-
-// const routes = [
-//   {
-//     path: '/characters',
-//     component: CharacterView,
-//     beforeEnter: requireAuth
-//   }
-// ]
-
-
-// // auth.js
-// import { defineStore } from 'pinia';
-
-// export const useAuthStore = defineStore('auth', {
-//   state: () => ({
-//     isAuthenticated: !!localStorage.getItem('token'), // Vérifie si un token est présent dans le localStorage
-//     token: localStorage.getItem('token') || null, // Stocke le token depuis le localStorage s'il existe
-//     // Autres états d'authentification si nécessaire
-//   }),
-//   actions: {
-//     // Action de connexion avec la gestion du token
-//     login(token) {
-//       this.isAuthenticated = true;
-//       this.token = token;
-//       localStorage.setItem('token', token); // Enregistre le token dans le localStorage
-//     },
-//     // Action de déconnexion
-//     logout() {
-//       this.isAuthenticated = false;
-//       this.token = null;
-//       localStorage.removeItem('token'); // Supprime le token du localStorage lors de la déconnexion
-//     },
-//     // Vérifier l'authentification
-//     checkAuth() {
-//       if (this.token && !this.isAuthenticated) {
-//         this.isAuthenticated = true;
-//       }
-//     },
-//   },
-// });
